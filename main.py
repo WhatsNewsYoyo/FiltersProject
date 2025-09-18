e1212d95e8d0b64c86d20a5f48d92114698edb0c
José Yael Varela García <132051688+WhatsNewsYoyo@users.noreply.github.com>
2025-09-18 11:52:00 -0600

    function def

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pytesseract
 
+
+
+
+#Functions to add
+def checkColor(image):
+    
+    
+    
+    
+def checkVertex(image):
+    
+    
+
+
+
+images = ["dangerSign.jpg", "stopSign.jpg", "WheelchairSign.png"]
+n = len(images)
+for i in range (0,n) :
