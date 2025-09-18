5f7f7e756a6d8897d6bf64bed48ffd458d7dff31
José Yael Varela García <132051688+WhatsNewsYoyo@users.noreply.github.com>
2025-09-18 13:27:02 -0600

    Changes for detection in the number of vertex

--- main.py
+++ main.py
@@ -7,36 +7,32 @@ import pytesseract
 #Functions to add
 def checkColor(image):
     i = 0;
-    return i
     
     
     
 def checkVertex(image):
     img = cv2.imread(image)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gaussian = cv2.GaussianBlur(img_gray, (3,3), 1)
-
     _, thresh = cv2.threshold(img_gray, 240, 255, cv2.CHAIN_APPROX_NONE)
     contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     contour = max(contours, key=cv2.contourArea)
     perimeter = cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)
+    approx = cv2.approxPolyDP(contour, 0.04 * perimeter, True)       
     num_vertices = len(approx)
     print(f"Vertex numbers : {num_vertices}")
 
-
     #Screenshots for presentation
-    plt.figure(figsize=(1,10))
+    """
+    plt.figure(figsize=(1,3))
     plt.subplot(1,3,1)
     plt.imshow(cv2.cvtColor(img_gray, cv2.COLOR_BGR2RGB))
     plt.title("Gray scale")
     plt.axis('off')
 
-    plt.subplot(1,3,2)
-    plt.imshow(cv2.cvtColor(gaussian, cv2.COLOR_BGR2RGB))
-    plt.title("Gaussian and gray scale")
-    plt.axis('off')
     plt.show()
+    """
+
+    return num_vertices
 
 
 
